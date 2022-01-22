a3a93488e5a06ece99d1fa657d0486fc7272475c
Diego Niquefa <diego@valiu.co>
2022-01-21 19:48:03 -0500

    update template

--- notebook/template.go
+++ notebook/template.go
@@ -11,11 +11,15 @@ var (
 	out = bufio.NewWriter(os.Stdout)
 )
 
+func scan(a ...interface{})             { fmt.Fscan(in, a...) }
+func printf(f string, a ...interface{}) { fmt.Fprintf(out, f, a...) }
+
 func main() {
+	defer out.Flush()
+
 	var ntc int
 	fmt.Fscan(in, &ntc)
 	for t := 0; t < ntc; t++ {
 
 	}
-	out.Flush()
 }
