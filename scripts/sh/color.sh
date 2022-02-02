38670f5ebf83edac17193afc4ccf8b3dd25443bb
Diego Niquefa <diego@valiu.co>
2022-02-01 19:05:42 -0500

    update create script manual with instructions for polygon

--- scripts/sh/color.sh
+++ scripts/sh/color.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-TextReset="\033[0m"       # Text Reset
+# https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+
+Normal="\033[0m"          # Reset to normal font
 Cyan="\033[0;36m"         # Cyan
 Red="\033[0;31m"          # Red
 Green="\033[0;32m"        # Green
+Bold="\033[1m"            # Bold
+Italic="\033[3m"            # Bold
 
-EchoError() { echo "${Red}ERROR: $1${TextReset}"; }
-EchoRed() { echo "${Red}$1$TextReset"; }
-EchoGreen() { echo "${Green}$1${TextReset}"; }
-EchoInfo() { echo "${Cyan}$1${TextReset}"; }
-EchoInfoNoEndl()  { printf "${Cyan}%s${TextReset}" "$1";}
+EchoError() { echo "${Red}ERROR: $1${Normal}"; }
+EchoRed() { echo "${Red}$1$Normal"; }
+EchoGreen() { echo "${Green}$1${Normal}"; }
+EchoInfo() { echo "${Cyan}$1${Normal}"; }
+EchoInfoNoEndl()  { printf "${Cyan}%s${Normal}" "$1";}
 
 SetInfoColor() { printf "${Cyan}"; }
-ResetColor() { printf "${TextReset}"; }
+ResetColor() { printf "${Normal}"; }
