f226b4ce10c44a0158842f4405898a0e8c295d42
Diego Niquefa <niquefa.diego@gmail.com>
2022-02-07 00:00:07 -0500

    add missing colon

--- scripts/sh/docs/show-polygon-generator-manual.sh
+++ scripts/sh/docs/show-polygon-generator-manual.sh
@@ -11,6 +11,6 @@ echo "Compile and run a generator of the current problem. More specifically, com
 echo "workspace/${Bold}Problem${Normal}/polygon/${Bold}GeneratorName${Normal}.cpp and run it with ${Bold}[GeneratorArgs..]${Normal}"
 echo "as command line arguments"
 echo ""
-echo "${Bold}Options${Normal}"
+echo "${Bold}Options${Normal}:"
 echo "  ${Bold}-c${Normal}, ${Bold}--case arg${Normal}           save the generated test case with the given ID"
 echo "  ${Bold}-h${Normal}, ${Bold}--help${Normal}               show this manual, then exit"
