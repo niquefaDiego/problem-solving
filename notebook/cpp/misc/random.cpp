ee5fedaec13b405b9699840eb9a1cf0418339e43
Diego Niquefa <diego@valiu.co>
2022-02-08 13:47:41 -0500

    add misc random to c++ notebook

---
+++ notebook/cpp/misc/random.cpp
@@ -0,0 +1,14 @@
+
+unsigned int g_seed = 1000000021;
+const unsigned int FASTRAND_MAX = 0x7FFF;
+inline int fastrand() {
+    g_seed = (214013*g_seed+2531011);
+	return (g_seed>>16)&0x7FFF;
+}
+
+template<class T>
+void premutateRandom(vector<T>& v) {
+  for (int i = 1; i < v.size(); i++) {
+    swap(v[i], v[fastrand()%i]);
+  }
+}
