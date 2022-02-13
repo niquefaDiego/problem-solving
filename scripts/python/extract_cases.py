bb7c3ebb9b3eb1ff315644c17c55f3b036557464
Diego Niquefa <niquefa.diego@gmail.com>
2022-02-13 18:21:23 -0500

    use 2 spaces as tab

--- scripts/python/extract_cases.py
+++ scripts/python/extract_cases.py
@@ -3,22 +3,21 @@ import os
 from bs4 import BeautifulSoup
 
 def write_file(path_file, content):
-    f = open(path_file, "w+")
-    f.write(content)
-    f.close()
+  f = open(path_file, "w+")
+  f.write(content)
+  f.close()
 
 def get_text(soup_obj):
-    return soup_obj.getText().strip()
+  return soup_obj.getText().strip()
 
 def my_quit():
-    print(-1)
-    quit()
+  quit()
 
 if ( len(sys.argv) > 1):
-    path = sys.argv[1]
+  path = sys.argv[1]
 else:
-    print("ERROR! No path given") 
-    my_quit()
+  print("ERROR! No path given") 
+  my_quit()
 
 cwd = os.getcwd()
 cases_path = "/".join([cwd, path])
@@ -30,15 +29,15 @@ with open(cwd+"/tmp/problem.html") as fp:
 
 test_cases = soup.find_all(class_="sample-tests")
 if ( len(test_cases) == 0 ):
-    myQuit()
+  myQuit()
 
 inputs = test_cases[0].find_all(class_="input")
 outputs = test_cases[0].find_all(class_="output")
 t = len(inputs)
 
 for i in range(t):
-    file_path = cases_path + str(i)
-    write_file(file_path+".in", get_text(inputs[i].pre)) 
-    write_file(file_path+".out", get_text(outputs[i].pre)) 
+  file_path = cases_path + str(i)
+  write_file(file_path+".in", get_text(inputs[i].pre)) 
+  write_file(file_path+".out", get_text(outputs[i].pre)) 
 
 print(t)
