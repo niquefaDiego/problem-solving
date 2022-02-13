f8c15cc63c8bf12a6d47b4a398afbc252dded646
Diego Niquefa <niquefa.diego@gmail.com>
2022-02-12 23:36:06 -0500

    update c++ template

--- notebook/cpp/template.cpp
+++ notebook/cpp/template.cpp
@@ -1,10 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) 42
+#endif
+
 int main() {
-  int ntc;
-  scanf("%d", &ntc);
-  while (ntc--) {
-  }
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+
   return 0;
 }
---
+++ scripts/cpp/polygon/cpp-include-path/debug.h
@@ -0,0 +1,38 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+std::vector<std::string> vec_splitter(std::string s) {
+	s += ',';
+	std::vector<std::string> res;
+	while(!s.empty()) {
+		res.push_back(s.substr(0, s.find(',')));
+		s = s.substr(s.find(',') + 1);
+	}
+	return res;
+}
+
+void debug_out(
+	std::vector<std::string> __attribute__ ((unused)) args,
+	__attribute__ ((unused)) int idx, 
+	__attribute__ ((unused)) int LINE_NUM
+) {
+	std::cerr << std::endl;
+} 
+
+template <typename Head, typename... Tail>
+void debug_out(
+	vector<string> args,
+	int idx,
+	int LINE_NUM,
+	Head H,
+	Tail... T
+) {
+	if(idx > 0) cerr << ", ";
+	else cerr << "Line(" << LINE_NUM << ") ";
+	stringstream ss; ss << H;
+	cerr << args[idx] << " = " << ss.str();
+	debug_out(args, idx + 1, LINE_NUM, T...);
+}
+
+#define debug(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
