fec90bdde1005a6dd1c24e4c3f8a7d6864cd46ce
Diego Niquefa <diego@valiu.co>
2022-02-16 21:35:48 -0500

    do not run validator if compilation fails

--- scripts/sh/compile-cpp.sh
+++ scripts/sh/compile-cpp.sh
@@ -12,11 +12,15 @@ mkdir -p "$ExecFolder"
 if cmp --silent "$CppFile" "$CacheFile"; then
   EchoInfo "No changes in $CppFile"
 else
+  if [ -f "$ExecFile" ]; then
+    rm "$ExecFile"
+  fi
+
+  cp "$CppFile" "$CacheFile"
   EchoInfo "Compiling $CppFile"
   g++ "$CppFile" -std=c++17 -o "$ExecFile" -I "scripts/cpp/polygon/cpp-include-path"
   if [ $? -ne 0 ]; then
     EchoError "Compilation error :("
     exit 1
   fi
-  cp "$CppFile" "$CacheFile"
 fi
