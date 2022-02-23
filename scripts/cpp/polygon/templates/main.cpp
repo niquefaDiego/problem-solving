c9c9a62ae18ae61b25077961adb76db68e70fe84
Diego Niquefa <niquefa.diego@gmail.com>
2022-02-23 14:10:27 -0500

    update polygon template

--- scripts/cpp/polygon/templates/main.cpp
+++ scripts/cpp/polygon/templates/main.cpp
@@ -1,9 +1,18 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) 42
+#endif
 using namespace std;
 
 int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+
   int a, b;
   cin >> a >> b;
   cout << a+b << "\n";
+
   return 0;
 }
