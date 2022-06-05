991299aa1cb6a2f3116731eeb0b5bf0d6bf67be0
Diego Niquefa <niquefa.diego@gmail.com>
2022-06-04 21:58:30 -0500

    fix class name to match file name

--- notebook/java/template.java
+++ notebook/java/template.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
  
-public class Main {
+public class main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int ntc = in.nextInt();
